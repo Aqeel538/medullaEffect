6ee078522d7a777775357c91c2118ea1dc986e20
NaumanCS <nauman@codingsolution24.com>
2023-01-09 14:41:56 +0500

    Setup

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB, Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+        'email' =>'admin@admin.com',
+        'password' => Hash::make('12345678'),
+        'role' => 'admin'
+        ]);
+    }
+}
